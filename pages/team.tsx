552e9a5ae93064ec340dca3fea278d85ceaec565
tushar <tusharkumarshah14394@gmail.com>
2024-02-28 23:27:49 +0530

    our patron adn mentor added

--- pages/team.tsx
+++ pages/team.tsx
@@ -53,9 +53,38 @@ function team() {
           />
 
           <h2 className="font-clashDisplay  font-medium text-5xl md:text-6xl text-center text-hsWhite mb-24">
+            Our Patrons
+          </h2>
+          <div className={`grid grid-cols-1 gap-y-7  sm:grid-cols-3   gap-x-5 `}>
+          
+          <Card
+              image="/Team/Harjot.jpeg"
+              name="Dr. Harjot Singh Gill"
+              designation="Director, E-Governance"
+            />
+            <Card
+              image="/Team/jagdeep.jpeg"
+              name="Dr. Jagdeep Singh"
+              designation="Associate Director, E-Governance"
+            />
+            <Card
+              image="/Team/sumit.jpeg"
+              name="Dr. Sumit Sharma"
+              designation="Assistant Director, E-Governance"
+            />
+          </div>
+          
+        </section>
+        <section className="border-t py-24 relative isolate ">
+        <h2 className="font-clashDisplay  font-medium text-5xl md:text-6xl text-center text-hsWhite mb-24">
             Our Mentors
           </h2>
-          <div className={`grid grid-cols-1 gap-y-7  sm:grid-cols-2   gap-x-5 `}>
+          <div className={`grid grid-cols-1 gap-y-7  sm:grid-cols-3   gap-x-5 `}>
+            <Card
+              image="/Team/sachin.jpg"
+              name="Dr. Sachin Ahuja"
+              designation="Executive Director, Chandigarh University"
+            />
             <Card
               image="/Team/Aa.jpg"
               name="Dr. Abhineet Anand"
@@ -69,9 +98,6 @@ function team() {
           </div>
         </section>
         <section className="border-t py-24 relative isolate ">
-
-
-
           <h2 className="font-clashDisplay  font-medium text-5xl md:text-6xl text-center text-hsWhite mb-24">
             Founding Mentors
           </h2>
